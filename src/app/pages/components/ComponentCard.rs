d065d308f8ec277d792a474401290ec3dffa5821
Kiril Ivanov <kiril.ivanov91@gmail.com>
2024-09-15 22:59:08 +0300

    feature: components api support
    Fixing styles and sizes in ComponentCard
    Adding "arrow" button in ComponentCard, to be used for toggling a short component info preview.

--- src/app/pages/components/ComponentCard.rs
+++ src/app/pages/components/ComponentCard.rs
@@ -11,22 +11,53 @@ pub struct ComponentProps {
 pub fn ComponentCard(props: ComponentProps) -> Element {
     rsx! {
         div {
-            class: "relative flex flex-row flex-wrap w-48 h-52 p-4 rounded-xl bg-zinc-900",
+            class: "relative flex flex-row flex-wrap w-48 h-52 p-4 rounded-xl bg-zinc-900 border-neutral-500",
             div {
-                class: "component__card__header w-40 h-32",
+                class: "component__card__header w-40 h-28",
                 img {
                     class:
-                    if(props.component.component_image == ""){ "w-40 h-32 border border-white" }
-                    else {"w-40 h-32"},
+                    if(props.component.component_image == ""){ "w-40 h-28 border text-neutral-500 border-neutral-500" }
+                    else {"w-40 h-28"},
                     src: "{props.component.component_image}",
                     alt: "No img"
                 }
             }
             div {
-                class: "component__card__footer w-40 h-12 py-4 overflow-hidden text-ellipsis",
+                class: "component__card__footer flex flex-row w-40 h-12 pt-4",
                 span {
-                    class: "",
+                    class: "component__name w-40 overflow-hidden text-ellipsis",
                     "{props.component.component_name}"
+                }
+                button {
+                    class: "w-8",
+                    svg {
+                        class: "svg__chevron__right size-6",
+                        xmlns: "http://www.w3.org/2000/svg",
+                        fill: "none",
+                        view_box: "0 0 24 24",
+                        stroke_width: "1.5",
+                        stroke: "currentColor",
+                        path {
+                            stroke_linecap: "round",
+                            stroke_linejoin: "round",
+                            d: "m8.25 4.5 7.5 7.5-7.5 7.5",
+                        }
+                    },
+                    //   //TODO this is the "left" icon snippet, when info box opened, need to be implemented
+                        // svg {
+                        //     class: "svg__chevron__left size-6",
+                        //     xmlns: "http://www.w3.org/2000/svg",
+                        //     fill: "none",
+                        //     view_box: "0 0 24 24",
+                        //     stroke_width: "1.5",
+                        //     stroke: "currentColor",
+                        //     path{
+                        //         stroke_linecap: "round",
+                        //         stroke_linejoin:"round",
+                        //         d: "M15.75 19.5 8.25 12l7.5-7.5",
+                        //     }
+                        // }
+
                 }
             }
         }
