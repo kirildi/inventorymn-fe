0ec24692bdac6ea4189dedc3ad6aae668c7fe191
Kiril Ivanov <kiril.ivanov91@gmail.com>
2025-06-18 23:20:47 +0300

    feature: add projects page
    Adding ProjectPage and ProjectList components

--- src/app/pages/ProjectsPage.rs
+++ src/app/pages/ProjectsPage.rs
@@ -5,63 +5,67 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use uuid::Uuid;
 
-use crate::app::pages::components::{LocationList::LocationList, SideNav::SideNav};
+use crate::app::pages::components::{ProjectList::ProjectList, SideNav::SideNav};
 
 #[derive(PartialEq, Clone, Serialize, Deserialize, Debug, Props)]
-pub struct Location {
-    pub location_id: Uuid,
-    pub location_name: String,
-    pub location_capacity: i64,
+pub struct Project {
+    pub project_id: Uuid,
+    pub project_name: String,
+    pub project_description: String,
+    pub project_image: String,
+    pub create_date: String,
+    pub finish_date: String,
+    pub project_status: bool,
+    pub project_progress: i64,
     pub user_id: Uuid,
 }
 
-pub fn LocationsPage() -> Element {
+pub fn ComponentsPage() -> Element {
     let api_client: Signal<reqwest::Client> = use_context::<Signal<reqwest::Client>>();
-    let mut locations_list: Signal<Vec<Location>> = use_signal(|| Vec::<Location>::new());
+    let mut components_list: Signal<Vec<Project>> = use_signal(|| Vec::<Project>::new());
 
-    // TODO this hardcoded id is used while developing, should be removed on all places
     let mut request_data = HashMap::new();
     request_data.insert(
         String::from("user_id"),
         String::from("b845f7a7-cac0-4879-9ea2-bf685cdf7259"),
     );
 
-    let fetch_locations = use_resource(move || {
+    let fetch_projects = use_resource(move || {
         let value = request_data.clone();
         async move {
             let mut result = api_client()
-                .get("http://localhost:3000/locations")
+                .get("http://localhost:3000/projects")
                 .json(&value)
                 .send()
                 .await
                 .unwrap()
-                .json::<Vec<Location>>()
+                .json::<Vec<Project>>()
                 .await;
             result
         }
     });
 
-    let locations_wrapper: Result<VNode, RenderError> = match &*fetch_locations.read() {
-        Some(Ok(response_location_list)) => {
+    let projects_wrapper: Result<VNode, RenderError> = match &*fetch_projects.read() {
+        Some(Ok(response_projects_list)) => {
             rsx! {
-                LocationList { location_list: response_location_list.clone()}
+                ProjectList { project_list: response_projects_list.clone()}
             }
         }
         Some(Err(err)) => {
             tracing::warn!("BAD request !!!, {:?}", err);
             rsx! {
                 h1 {
-                    "Internal Error! No Locations found."
+                    "Internal Error! No Projects found."
                 }
             }
         }
-        None => rsx! { h1 {"Loading locations..."} },
+        None => rsx! { h1 {"Loading projects..."} },
     };
 
     rsx! {
         section {
-            class: "locations__section flex flex-row flex-wrap gap-4 w-3/4 h-[34rem] p-4 text-xl rounded-xl bg-zinc-800",
-            {locations_wrapper}
+            class: "projects__section flex flex-row flex-wrap gap-4 w-3/4 h-[34rem] p-4 text-xl rounded-xl bg-zinc-800",
+            {projects_wrapper}
         },
         section {
             class:"side__nav h-full w-1/4 text-xl rounded-xl",
--- src/app/pages/components.rs
+++ src/app/pages/components.rs
@@ -1,6 +1,7 @@
 pub mod ComponentCard;
 pub mod CreateButton;
 pub mod LocationList;
+pub mod ProjectList;
 pub mod SideNav;
 pub mod StatusBox;
 pub mod forms;
