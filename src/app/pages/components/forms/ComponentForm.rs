e5105d6f8bd68bdaabaf1bd8be7abb904497ebe3
Kiril Ivanov <kiril.ivanov91@gmail.com>
2025-08-21 00:01:26 +0300

    fix: bug with missing field
    Fixing bug of missing field for vendor in ComponentForm.
    The bug is causing error in new Component creation query, because field is required
    
    Closing #38

--- src/app/pages/components/forms/ComponentForm.rs
+++ src/app/pages/components/forms/ComponentForm.rs
@@ -11,9 +11,10 @@ pub fn ComponentForm() -> Element {
     let mut component_name = use_signal(|| String::from(""));
     let mut component_description = use_signal(|| String::from(""));
     let mut component_image = use_signal(|| String::from(""));
+    let mut component_vendor = use_signal(|| String::from(""));
     let mut install_date = use_signal(|| String::from(""));
 
-    let mut create_component = move |event: Event<MouseData>| {
+    let mut create_component = move |event: Event<FormData>| {
         let mut form_data = HashMap::new();
         form_data.insert(String::from("component_name"), component_name());
         form_data.insert(
@@ -21,6 +22,7 @@ pub fn ComponentForm() -> Element {
             component_description(),
         );
         form_data.insert(String::from("component_image"), component_image());
+        form_data.insert(String::from("component_vendor"), component_vendor());
         form_data.insert(String::from("installed"), String::from(""));
         form_data.insert(String::from("status"), String::from(""));
         form_data.insert(String::from("install_date"), String::from(""));
@@ -38,22 +40,22 @@ pub fn ComponentForm() -> Element {
             String::from("00000000-0000-0000-0000-000000000000"),
         );
 
-        let json_data = serde_json::to_string(&form_data).unwrap();
-
         spawn(async move {
             let mut result = api_client()
-                .post("http://localhost:3000/components/create")
-                .json(&json_data)
+                .post("http://localhost:3000/component/create")
+                .json(&form_data)
                 .send()
                 .await;
-            tracing::info!("[CREATE_COMPONENT_RESPONSE] result is {:?}", result);
+
+            let response = result.unwrap();
+            tracing::info!("[CREATE_COMPONENT_RESPONSE] response is {:?}", response);
             tracing::info!("[CREATE_COMPONENT_FORM]  {:?}", form_data);
 
-            match result.unwrap().error_for_status() {
+            match response.error_for_status() {
                 Ok(_res) => {
-                    tracing::info!("COMPONENT CREATED !!!")
+                    tracing::info!("COMPONENT CREATED !!!");
                 }
-                Err(err) => tracing::warn!("BAD request !!!"),
+                Err(err) => tracing::warn!("BAD request !!!. {:?}", err),
             }
         });
     };
@@ -61,6 +63,7 @@ pub fn ComponentForm() -> Element {
     rsx! {
         form {
             class: "flex flex-wrap gap-4 w-full",
+            onsubmit: create_component,
             // First row
             div {
                 class: "flex w-full justify-between",
@@ -127,6 +130,8 @@ pub fn ComponentForm() -> Element {
                         class: "w-64 h-12 rounded-lg appearance-none block py-3 px-4 mb-3 focus:outline-none focus:bg-zinc-900 placeholder-zinc-600 text-base",
                         placeholder: "leave blank for None",
                         r#type: "text",
+                        onchange: move |event| component_vendor.set(event.value()),
+                        value: "{component_vendor}"
                     }
                 },
             },
@@ -185,7 +190,6 @@ pub fn ComponentForm() -> Element {
             div {
                 class: "flex gap-8 w-full items-center justify-center",
                 input {
-                    onclick: create_component,
                     class: "w-24 h-16 p-4 rounded-lg bg-zinc-900 cursor-pointer",
                     r#type: "submit",
                     "Create"
