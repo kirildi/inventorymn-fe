c95a07da8cb6cd8f372f9bde79da7350fa64eeba
Kiril Ivanov <kiril.ivanov91@gmail.com>
2025-12-07 23:37:35 +0200

    refactor: SingleComponentPage styling
    Adding placeholders and flex/grid "containers" for the component properties

--- src/app/pages/SingleComponentPage.rs
+++ src/app/pages/SingleComponentPage.rs
@@ -12,22 +12,29 @@ pub struct SingleComponentProps {
 pub fn SingleComponentPage(props: SingleComponentProps) -> Element {
     rsx!(
         div {
-            class: "",
+            class: "grid grid-rows-2 gap-4 bg-neutral-900 rounded-xl",
             div {
-                class: "single__component__header flex justify-around text-2xl w-full h-16 p-4 bg-zinc-700",
+                class: "single__component__header grid grid-cols-[16rem_1fr] grid-rows-[repeat(minmax(0, 4rem))] w-full p-4",
+
+                img {
+                    class: "w-64 h-64 col-span-1 row-span-3 p-4 rounded-xl border-2 border-neutral-600",
+                    src: "{props.component.component_image}",
+                    alt: "Component image"
+                },
                 div {
-                    class:"single__component__title",
+                    class: "single__component__title col-start-2 row-start-2 text-2xl h-16 p-4 pl-8",
                     h2 {
+                        class: "",
                         "{props.name}"
                     }
-                    div {
-                        class: "single__component__controls",
-                        ul {
-                            li { },
-                            li { },
-                        }
-                    },
-                }
+                },
+                div {
+                    class: "single__component__controls col-start-2 row-start-3 p-4 pl-8",
+                    ul {
+                        li { button { value: "copy"}},
+                        li { button { value: "delete"}},
+                    }
+                },
             }
             div {
                 class: "single__component__body w-full",
