c79ba502dc5339f27715daeaadc0481c8fe5a378
Kiril Ivanov <kiril.ivanov91@gmail.com>
2025-12-15 21:57:42 +0200

    refactor: SingleComponentPage
    Adding icons to the control buttons

--- src/app/pages/SingleComponentPage.rs
+++ src/app/pages/SingleComponentPage.rs
@@ -10,29 +10,65 @@ pub struct SingleComponentProps {
 }
 
 pub fn SingleComponentPage(props: SingleComponentProps) -> Element {
+    let component_image = match (props.component.component_image != "".to_string()) {
+        true => rsx! {
+            img {
+                class: "w-64 h-64 rounded-xl",
+                src: "{props.component.component_image}",
+                alt: "Component image"
+            }
+        },
+        false => rsx! {
+            div {
+                class: "w-full h-full flex justify-center items-center",
+                i {
+                    class: "ph ph-image-broken text-3xl"
+                },
+                h3 {
+                    "Image unavailable"
+                }
+            }
+        },
+    };
+
     rsx!(
         div {
             class: "grid grid-rows-2 gap-4 bg-neutral-900 rounded-xl",
             div {
                 class: "single__component__header grid grid-cols-[16rem_1fr] grid-rows-[repeat(minmax(0, 4rem))] w-full p-4",
 
-                img {
-                    class: "w-64 h-64 col-span-1 row-span-3 p-4 rounded-xl border-2 border-neutral-600",
-                    src: "{props.component.component_image}",
-                    alt: "Component image"
-                },
                 div {
-                    class: "single__component__title col-start-2 row-start-2 text-2xl h-16 p-4 pl-8",
+                    class: "w-64 h-64 col-span-1 row-span-3 p-4 rounded-xl border-2 border-neutral-600",
+                    {component_image}
+                }
+
+                div {
+                    class: "single__component__title col-start-2 row-start-2 text-2xl h-16 p-4 pl-8 overflow-hidden whitespace-nowrap",
                     h2 {
-                        class: "",
+                        class: "text-ellipsis",
                         "{props.name}"
                     }
                 },
                 div {
-                    class: "single__component__controls col-start-2 row-start-3 p-4 pl-8",
+                    class: "single__component__controls col-start-2 row-start-3 p-4 pl-8 rounded-xl",
                     ul {
-                        li { button { value: "copy"}},
-                        li { button { value: "delete"}},
+                        class: "flex flex-nowrap gap-4",
+                        li {
+                            class: "inline-block",
+                            button {
+                                class: "px-[0.4rem] py-[0.6rem] rounded-lg border-2 border-neutral-600",
+                                value: "copy",
+                                i { class:"ph ph-copy text-3xl" }
+                            }
+                        },
+                        li {
+                            class: "inline-block",
+                            button {
+                                class: "px-[0.4rem] py-[0.6rem] rounded-lg border-2 border-neutral-600",
+                                value: "delete",
+                                i { class: "ph ph-trash text-3xl" }
+                            }
+                        },
                     }
                 },
             }
